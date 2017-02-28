fc983a5dd6ce9dbe03eedc6b21170ea27a91700d
YongXin Shi <shiyongxin@aliyun.com>
2017-02-28 21:19:58 +0800

    index.php

--- index.php
+++ index.php
@@ -28,25 +28,25 @@
    <?php echo "今天是 " . date("Y-m-d  l") . "<br>"; ?>
   </div>
   <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  <br />
-  KEY: <input type="text" name="fname">
-  <input type="submit" value="Start">
+    <br />
+    KEY: <input type="text" name="fname">
+    <input type="submit" value="Start">
   </form>
   <?php $name = $_POST['fname'];
   if($name==="robotteam") {
-  	echo $name;
-	 $myfile = fopen("exec.txt", "w") or die("Unable to open file!");
-	 $txt = "key";
-  fwrite($myfile, $txt);
-  fclose($myfile);
+    echo $name;
+    $myfile = fopen("exec.txt", "w") or die("Unable to open file!");
+    $txt = "key";
+    fwrite($myfile, $txt);
+    fclose($myfile);
   //	system('./key.py');
   } else {
-  		echo "error";
-   	} ?>
+  	echo "error";
+  } ?>
    	<div class="text-center">
-   	<a href="./elFinder-2.x/elfinder.html">帮助</a>-<a href="http://59.72.114.29">首页</a>
-   	<hr>
-   	<div>JRT 唐敖庆B512号</div>
+   	  <a href="./elFinder-2.x/elfinder.html">帮助</a>-<a href="http://59.72.114.29">首页</a>
+   	  <hr>
+   	  <div>JRT 唐敖庆B512号</div>
    	</div>
   </div>
 </div>
--- one_key.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "deb http://mirrors.ustc.edu.cn/raspbian/raspbian/ wheezy main non-free contrib
-deb-src http://mirrors.ustc.edu.cn/raspbian/raspbian/ wheezy main non-free contrib" > /etc/apt/sources.list && echo "update sources to ustc"
