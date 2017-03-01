906556dfd2a109050c2362cb28523ecd469ce474
YongXin Shi <shiyongxin@aliyun.com>
2017-03-01 21:59:24 +0800

    add user.php & text.php

---
+++ text.php
@@ -0,0 +1,5 @@
+<?php
+$header_title = 'JRT_key';
+$footer = 'JRT 唐敖庆B512号';
+//$header_title = 'JRT_key'
+?>
---
+++ user.php
@@ -0,0 +1,10 @@
+<?php
+function user($value='')
+{
+  if($value === "robotteam") {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+?>
