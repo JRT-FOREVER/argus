017ef8ed2c247b5976d2c19ebc245f3e11c6a08c
YongXin Shi <shiyongxin@aliyun.com>
2017-03-02 21:23:49 +0800

    add page

--- text.php
+++ text.php
@@ -1,5 +1,8 @@
 <?php
-$header_title = 'JRT_key';
-$footer = 'JRT 唐敖庆B512号';
-//$header_title = 'JRT_key'
+$text_title = 'JRT_key';
+$text_footer = 'JRT 唐敖庆B512号';
+$text_today = '今天是';
+
+$img_logo = "image/JRT_logo.jpg";
+$img_main = "image/JRT_well*e.jpg"
 ?>
