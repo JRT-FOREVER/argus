e9af550cfa9a5a1ff955583cbc54defa065fefe2
a-wing <shiyongxin@aliyun.com>
2019-03-12 22:34:56 +0800

    Fix reboot bug

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,3 +4,6 @@ echo === init ===
 echo 2 > /sys/class/gpio/export
 echo 3 > /sys/class/gpio/export
 
+echo out > /sys/class/gpio/gpio2/direction
+echo out > /sys/class/gpio/gpio3/direction
+
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 echo === run ===
 
-echo in > /sys/class/gpio/gpio3/direction
+echo 1 > /sys/class/gpio/gpio2/value
+echo 0 > /sys/class/gpio/gpio3/value
+
 sleep 1.25
-echo out > /sys/class/gpio/gpio3/direction
-
+echo 0 > /sys/class/gpio/gpio2/value
+echo 1 > /sys/class/gpio/gpio3/value
 
 sleep 1
-
-
-echo in > /sys/class/gpio/gpio2/direction
-sleep 1
-echo out > /sys/class/gpio/gpio2/direction
+echo 0 > /sys/class/gpio/gpio2/value
+echo 0 > /sys/class/gpio/gpio3/value
 
